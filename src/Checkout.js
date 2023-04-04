afa67ce449f066119a57899d0b31e2e546534bec
Andrew Owino <andystudy4@gmail.com>
2023-04-04 03:56:36 +0300

    Write skeleton view with headers

---
+++ src/Checkout.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+
+function Checkout() {
+
+    return (
+        <div>
+            <h1>PharmaPay</h1>
+            <h2>Inventory</h2>
+            <h2>Cart</h2>
+            <button>Complete Sale</button>
+        </div>
+    )
+}
+
+export default Checkout;
