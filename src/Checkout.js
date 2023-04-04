41b3e9ffca7d29e5f3ef2de265be3b893b660c31
Andrew Owino <andystudy4@gmail.com>
2023-04-04 04:22:46 +0300

    Fetch inventory list from backend

--- src/Checkout.js
+++ src/Checkout.js
@@ -2,10 +2,31 @@ import React, { useState, useEffect } from 'react';
 
 function Checkout() {
 
+    const [inventory, setInventory] = useState([]);
+    const [cart, setCart] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:8080/api/v1/stock-items')
+          .then(response => response.json())
+          .then(data => setInventory(data));
+      }, []);
+
+    const addToCart = (item) => {
+        setCart([...cart, item]);
+      }
+
     return (
         <div>
             <h1>PharmaPay</h1>
             <h2>Inventory</h2>
+            {inventory.map(item => (
+                <div key={item.id}>
+                <h3>{item.name}</h3>
+                <p>Price: {item.price}</p>
+                <button onClick={() => addToCart(item)}>Add to Cart</button>
+                </div>
+            ))}
+
             <h2>Cart</h2>
             <button>Complete Sale</button>
         </div>
