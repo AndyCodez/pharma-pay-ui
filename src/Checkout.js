3b9a05eb14e98b099bd5188f6f6d30b9975bb6bc
Andrew Owino <andystudy4@gmail.com>
2023-04-04 14:07:09 +0300

    Complete sale

--- src/Checkout.js
+++ src/Checkout.js
@@ -12,12 +12,35 @@ function Checkout() {
       }, []);
 
     const addToCart = (item) => {
-        setCart([...cart, item]);
+        const cartItem = {
+          name: item.name,
+          quantity: item.quantity
+        }
+        setCart([...cart, cartItem]);
       }
 
     const removeFromCart = (item) => {
         setCart(cart.filter(cartItem => cartItem.id !== item.id));
     }
+
+    const completeSale = () => {
+        // const data = {
+        //   cart: cart,
+        // }
+        const data = cart;
+    
+        fetch('http://localhost:8080/api/v1/bills', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+        }).then(response => {
+          // Clear cart and update inventory after successful checkout
+          setCart([]);
+        //   setInventory(response.json());
+        });
+      }
 
     return (
         <div>
@@ -34,14 +57,13 @@ function Checkout() {
             <h2>Cart</h2>
 
             {cart.map(item => (
-                <div key={item.id}>
+                <div key={item.name}>
                 <h3>{item.name}</h3>
                 <p>Price: {item.price}</p>
                 <button onClick={() => removeFromCart(item)}>Remove</button>
                 </div>
             ))}
-            
-            <button>Complete Sale</button>
+            <button onClick={() => completeSale()}>Complete Sale</button>
         </div>
     )
 }
