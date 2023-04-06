847d06b6cb2dd1fa378740e557bbe86fb98910c1
Andrew Owino <andystudy4@gmail.com>
2023-04-06 23:20:10 +0300

    Improve UX

--- src/pages/Inventory.js
+++ src/pages/Inventory.js
@@ -4,190 +4,242 @@ import { useCart } from "../context/CartProvider";
 import { useAuth } from "../context/AuthProvider";
 
 const Inventory = () => {
-  const { auth } = useAuth();
-  const { authToken } = auth;
+    const { auth } = useAuth();
+    const { authToken } = auth;
 
-  const { inventory, setInventory, setErrorMessage, errorMessage } = useCart();
+    const { inventory, setInventory, setErrorMessage, errorMessage } = useCart();
 
-  const [itemName, setItemName] = useState("");
-  const [editItemId, setEditItemId] = useState(0);
-  const [editItemName, setEditItemName] = useState("");
-  const [editItemPrice, setEditItemPrice] = useState(0);
-  const [editItemQuantity, setEditItemQuantity] = useState(0);
-  const [itemQuantity, setItemQuantity] = useState("");
-  const [itemPrice, setItemPrice] = useState("");
+    const [itemName, setItemName] = useState("");
+    const [editItemId, setEditItemId] = useState("");
+    const [editItemName, setEditItemName] = useState("");
+    const [editItemPrice, setEditItemPrice] = useState("");
+    const [editItemQuantity, setEditItemQuantity] = useState("");
+    const [itemQuantity, setItemQuantity] = useState("");
+    const [itemPrice, setItemPrice] = useState("");
 
-  const apiVersion = "/api/v1";
+    const apiVersion = "/api/v1";
 
-  const fetchInventory = async () => {
-    try {
-      const response = await axios.get(`${apiVersion}/stock-items`, {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${authToken}`,
-        },
-        withCredentials: true,
-      });
-      setInventory(response.data);
-    } catch (err) {
-      const errorResponse = JSON.parse(JSON.stringify(err?.response?.data));
-      setErrorMessage(errorResponse.errorMessages);
-    }
-  };
-
-  const handleCreateItem = async (e) => {
-    e.preventDefault();
-
-    const data = {
-      name: itemName,
-      price: itemPrice,
-      quantity: itemQuantity,
+    const fetchInventory = async () => {
+        try {
+            const response = await axios.get(`${apiVersion}/stock-items`, {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${authToken}`,
+                },
+                withCredentials: true,
+            });
+            setInventory(response.data);
+        } catch (err) {
+            const errorResponse = JSON.parse(JSON.stringify(err?.response?.data));
+            setErrorMessage(errorResponse.errorMessages);
+        }
     };
 
-    try {
-      const response = await axios.post(
-        `${apiVersion}/stock-items`,
-        JSON.stringify(data),
-        {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${authToken}`,
-          },
-          withCredentials: true,
+    const handleCreateItem = async (e) => {
+        e.preventDefault();
+
+        const data = {
+            name: itemName,
+            price: itemPrice,
+            quantity: itemQuantity,
+        };
+
+        try {
+            const response = await axios.post(
+                `${apiVersion}/stock-items`,
+                JSON.stringify(data),
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${authToken}`,
+                    },
+                    withCredentials: true,
+                }
+            );
+            const inventoryItem = response.data;
+            setInventory([...inventory, inventoryItem]);
+            setItemName("");
+            setItemPrice("");
+            setItemQuantity("");
+            setErrorMessage("");
+
+        } catch (err) {
+            if (err?.response?.status === 400) {
+                const errorResponse = JSON.parse(JSON.stringify(err?.response?.data));
+                setErrorMessage(errorResponse.errorMessages);
+            } else {
+                setErrorMessage("Something went wrong. Please retry.");
+            }
         }
-      );
-      const inventoryItem = response.data;
-      setInventory([...inventory, inventoryItem]);
-      setItemName("");
-      setItemPrice(0);
-      setItemQuantity(0);
-    } catch (err) {
-      if (err?.response?.status === 400) {
-        const errorResponse = JSON.parse(JSON.stringify(err?.response?.data));
-        setErrorMessage(errorResponse.errorMessages);
-      } else {
-        setErrorMessage("Something went wrong. Please retry.");
-      }
-    }
-  };
-
-  const handleUpdateItem = async (e) => {
-    e.preventDefault();
-
-    const updatedItem = {
-      name: editItemName,
-      price: editItemPrice,
-      quantity: editItemQuantity,
     };
 
-    try {
-      const response = await axios.put(
-        `${apiVersion}/stock-items/${editItemId}`,
-        JSON.stringify(updatedItem),
-        {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${authToken}`,
-          },
+    const handleUpdateItem = async (e) => {
+        e.preventDefault();
+
+        const updatedItem = {
+            name: editItemName,
+            price: editItemPrice,
+            quantity: editItemQuantity,
+        };
+
+        try {
+            const response = await axios.put(
+                `${apiVersion}/stock-items/${editItemId}`,
+                JSON.stringify(updatedItem),
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${authToken}`,
+                    },
+                }
+            );
+
+            fetchInventory();
+            emptyEditField();
+        } catch (err) {
+            if (err?.response?.status === 400) {
+                const errorResponse = JSON.parse(JSON.stringify(err?.response?.data));
+                setErrorMessage(errorResponse.errorMessages);
+            } else {
+                setErrorMessage("Something went wrong. Please retry.");
+            }
         }
-      );
+    };
 
-      fetchInventory();
-      emptyEditField();
-    } catch (err) {
-      if (err?.response?.status === 400) {
-        const errorResponse = JSON.parse(JSON.stringify(err?.response?.data));
-        setErrorMessage(errorResponse.errorMessages);
-      } else {
-        setErrorMessage("Something went wrong. Please retry.");
-      }
+    const abortUpdateItem = (e) => {
+        e.preventDefault();
+        emptyEditField();
     }
-  };
 
-  const handleDeleteItem = async (id) => {
-    try {
-      await axios.delete(`${apiVersion}/stock-items/${id}`, {
-        headers: { Authorization: `Bearer ${authToken}` },
-      });
-      const updatedItems = inventory.filter((item) => item.id !== id);
-      setInventory(updatedItems);
-    } catch (error) {
-      console.error(error);
+    const handleDeleteItem = async (id) => {
+        try {
+            await axios.delete(`${apiVersion}/stock-items/${id}`, {
+                headers: { Authorization: `Bearer ${authToken}` },
+            });
+            const updatedItems = inventory.filter((item) => item.id !== id);
+            setInventory(updatedItems);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const populateEditField = (item) => {
+        setEditItemId(item.id);
+        setEditItemName(item.name);
+        setEditItemPrice(item.price);
+        setEditItemQuantity(item.quantity);
+        scrollToTop();
+
+    };
+
+    const emptyEditField = () => {
+        populateEditField({ id: "", name: "", price: "", quantity: "" });
+        setErrorMessage("");
+    };
+
+    const scrollToTop = () => {
+        window.scroll({ top: 0, behavior: 'smooth' });
     }
-  };
 
-  const populateEditField = (item) => {
-    setEditItemId(item.id);
-    setEditItemName(item.name);
-    setEditItemPrice(item.price);
-    setEditItemQuantity(item.quantity);
-  };
+    return (
+        <>
+            <div className="p-4">
+                <h1 className="text-2xl font-bold mb-4">Inventory</h1>
+                <p className={`text-red-500 ${errorMessage ? "block" : "hidden"} mb-2`}>
+                    {errorMessage}
+                </p>
+                {editItemId === "" ? (
+                    <form className="mb-4" onSubmit={handleCreateItem}>
+                        <div className="flex flex-wrap -mx-2 mb-4">
+                            <div className="w-full md:w-1/3 px-2 mb-4 md:mb-0">
+                                <input
+                                    className="block w-full border-gray-400 border py-2 px-3 rounded-lg"
+                                    type="text"
+                                    placeholder="Item name"
+                                    value={itemName}
+                                    onChange={(event) => setItemName(event.target.value)}
+                                />
+                            </div>
+                            <div className="w-full md:w-1/3 px-2 mb-4 md:mb-0">
+                                <input
+                                    className="block w-full border-gray-400 border py-2 px-3 rounded-lg"
+                                    type="number"
+                                    placeholder="Item price"
+                                    value={itemPrice}
+                                    onChange={(event) => setItemPrice(event.target.value)}
+                                />
+                            </div>
+                            <div className="w-full md:w-1/3 px-2 mb-4 md:mb-0">
+                                <input
+                                    className="block w-full border-gray-400 border py-2 px-3 rounded-lg"
+                                    type="text"
+                                    placeholder="Item quantity"
+                                    value={itemQuantity}
+                                    onChange={(event) => setItemQuantity(event.target.value)}
+                                />
+                            </div>
+                        </div>
+                        <button className="bg-blue-500 text-white px-4 py-2 rounded-lg" type="submit">Add item</button>
+                    </form>
+                ) : (
+                    <form className="mb-4">
+                        <div className="flex flex-wrap -mx-2 mb-4">
+                            <div className="w-full md:w-1/3 px-2 mb-4 md:mb-0">
+                                <input
+                                    className="block w-full border-gray-400 border py-2 px-3 rounded-lg"
+                                    type="text"
+                                    value={editItemName}
+                                    onChange={(e) => setEditItemName(e.target.value)}
+                                />
+                            </div>
+                            <div className="w-full md:w-1/3 px-2 mb-4 md:mb-0">
+                                <input
+                                    className="block w-full border-gray-400 border py-2 px-3 rounded-lg"
+                                    type="number"
+                                    value={editItemPrice}
+                                    onChange={(e) => setEditItemPrice(e.target.value)}
+                                />
+                            </div>
+                            <div className="w-full md:w-1/3 px-2 mb-4 md:mb-0">
+                                <input
+                                    className="block w-full border-gray-400 border py-2 px-3 rounded-lg"
+                                    type="number"
+                                    value={editItemQuantity}
+                                    onChange={(e) => setEditItemQuantity(e.target.value)}
+                                />
+                            </div>
+                        </div>
+                        <button className="bg-blue-500 text-white px-4 py-2 rounded-lg" type="submit" onClick={(e) => handleUpdateItem(e)}>Submit Update</button>
+                        <button className="bg-orange-500 text-white px-4 py-2 rounded-lg" type="submit" onClick={(e) => abortUpdateItem(e)}>Abort Update</button>
+                    </form>
+                )}
+            </div>
 
-  const emptyEditField = () => {
-    populateEditField({ id: "", name: "", price: "", quantity: "" });
-    setErrorMessage("");
-  };
-
-  return (
-    <div>
-      <h1>Inventory</h1>
-      <p className={`${errorMessage ? "text-red-500" : "hidden"} mb-2`}>
-        {errorMessage}
-      </p>
-      <form onSubmit={handleCreateItem}>
-        <input
-          type="text"
-          placeholder="Item name"
-          value={itemName}
-          onChange={(event) => setItemName(event.target.value)}
-        />
-        <input
-          type="number"
-          placeholder="Item price"
-          value={itemPrice}
-          onChange={(event) => setItemPrice(event.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Item quantity"
-          value={itemQuantity}
-          onChange={(event) => setItemQuantity(event.target.value)}
-        />
-        <button type="submit">Add item</button>
-      </form>
-
-      <form onSubmit={(e) => handleUpdateItem(e)}>
-        <input
-          type="text"
-          value={editItemName}
-          onChange={(e) => setEditItemName(e.target.value)}
-        />
-        <input
-          type="number"
-          value={editItemPrice}
-          onChange={(e) => setEditItemPrice(e.target.value)}
-        />
-        <input
-          type="number"
-          value={editItemQuantity}
-          onChange={(e) => setEditItemQuantity(e.target.value)}
-        />
-        <button>Submit Update</button>
-      </form>
-
-      <ul>
-        {inventory.map((item) => (
-          <li key={item.id}>
-            Name: {item.name}
-            Price: {item.price}
-            Qty: {item.quantity}
-            <button onClick={(e) => populateEditField(item)}>Update</button>
-            <button onClick={(e) => handleDeleteItem(item.id)}>Delete</button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+            <table className="w-full text-center">
+                <thead>
+                    <tr>
+                        <th>Item name</th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {inventory.map((item) => (
+                        <tr key={item.id}>
+                            <td>{item.name}</td>
+                            <td>{item.price}</td>
+                            <td>{item.quantity}</td>
+                            <td>
+                                <button className="bg-blue-500 text-white px-4 py-2 rounded-lg mr-2" onClick={(e) => populateEditField(item)}>Update</button>
+                                <button className="bg-red-500 text-white px-4 py-2 rounded-lg" onClick={(e) => handleDeleteItem(item.id)}>Delete</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
+    );
 };
 
 export default Inventory;
